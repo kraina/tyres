5cefeefa5d0a3cdc3c1de1858e79b04d60aea6f7
Vera <uamailcom@gmail.com>
2020-07-01 13:24:38 +0300

    Init

---
+++ app/Http/Controllers/TyreController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tyre;
+use Illuminate\Http\Request;
+
+class TyreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tyre  $tyre
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tyre $tyre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tyre  $tyre
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tyre $tyre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tyre  $tyre
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tyre $tyre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tyre  $tyre
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tyre $tyre)
+    {
+        //
+    }
+}
